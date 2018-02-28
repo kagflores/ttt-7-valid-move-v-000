845ad3b206e7cfc2331779583eaf5ff7f9af6f8f
Karen Flores <karen.ag.flores@gmail.com>
2018-02-28 06:00:40 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,6 +5,7 @@ if index.between?(0,8)
 else
   nil
 end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken? (board, index_number)
